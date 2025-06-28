a1e37d9fdedf458d767f41cbdcb923d49b5643b3
iamsnaaz <sadiyanaaz4255@gmail.com>
2025-06-28 13:51:37 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
